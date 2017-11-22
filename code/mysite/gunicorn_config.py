4cfee0886276a18e9fb4c33db1a6d8158a0bb30b
Ishan Khare <ishan.khare@vegfru.com>
2017-11-22 14:50:52 +0530

    setup allowed hosts and bind gunicorn to port 8000
    Signed-off-by: Ishan Khare <ishan.khare@vegfru.com>

--- code/mysite/gunicorn_config.py
+++ code/mysite/gunicorn_config.py
@@ -1,2 +1,2 @@
-bind = '0.0.0.0:8000'   # change it to the port you want to bind to
+bind = '0.0.0.0:80'   # change it to the port you want to bind to
 workers = 3             # modify to desired number of workers
--- code/mysite/mysite/settings.py
+++ code/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kakgd+$h@dhhg)xz@@hs#7ua^dq36ijk1@z*8q&=44f78onain'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.188.215.192', 'localhost']
+ALLOWED_HOSTS = ['35.188.215.192', 'ishankhare.com', 'www.ishankhare.com']
 
 
 # Application definition
