55e97c7f64cd4597b8d782d2d611afa42b3d3fcd
Ishan Khare <ishankhare07@gmail.com>
2017-12-05 20:43:27 +0530

    testing if jquery available on mobile
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- code/mysite/static/js/home.js
+++ code/mysite/static/js/home.js
@@ -1,3 +1,15 @@
-window.onload = () => {
-	$("#navToggle").sideNav();
+// window.onload = () => {
+// 	$("#navToggle").sideNav();
+// }
+
+window.onload = function()
+{
+    if (window.jQuery)
+    {
+        alert('jQuery is loaded');
+    }
+    else
+    {
+        alert('jQuery is not loaded');
+    }
 }
