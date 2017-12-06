b943e8e68dee22db8756dcaabb1cfbb1d59e4019
Ishan Khare <ishankhare07@gmail.com>
2017-12-06 20:28:16 +0530

    sanitized script, add command for pulling the images
    this means new containers won't be built on compute instances, hence
    save $
    
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- post_ssh.sh
+++ post_ssh.sh
@@ -1,8 +1,21 @@
 mkdir blog1
 mv deployment.tar.gz blog1
-cd blog1; tar -zxvf deployment.tar.gz; cd $HOME;
-cd blog; docker-compose stop; cd $HOME;
-sudo rm -rf blog; mv blog1 blog;
-cd blog; docker-compose up -d django
-echo "Trying to run django migrate";
+cd blog1
+echo "inflating deployment"
+tar -zxvf deployment.tar.gz
+
+cd $HOME/blog
+echo "bringing down cluster"
+docker-compose stop
+cd $HOME
+
+sudo rm -rf blog
+mv blog1 blog
+cd blog
+
+echo "pulling latest docker images"
+docker-compose pull
+echo "bringing up cluster"
+docker-compose up -d django
+echo "Trying to run django migrate"
 docker exec -it ahins-prod /bin/bash -c "python3 manage.py migrate;echo running collectstatic; yes yes | python3 manage.py collectstatic -l"
