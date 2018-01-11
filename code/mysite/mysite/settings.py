4902f942bd524974ab886f31753cd3befe7eb250
Ishan Khare <ishankhare07@gmail.com>
2018-01-11 17:48:16 +0530

    remove unwanted static files app and static root
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- code/mysite/mysite/settings.py
+++ code/mysite/mysite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework'
 ]
 
@@ -144,4 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
