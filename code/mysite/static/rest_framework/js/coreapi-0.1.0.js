f6fbbf5161c9c56b7285e58f63e16ebe856cadb3
Ishan Khare <ishan.khare@rebit.org.in>
2018-08-24 17:07:43 +0530

    change http urls to //
    Signed-off-by: Ishan Khare <ishan.khare@rebit.org.in>

--- code/mysite/static/rest_framework/js/coreapi-0.1.0.js
+++ code/mysite/static/rest_framework/js/coreapi-0.1.0.js
@@ -936,7 +936,7 @@ exports.parse = querystring;
 /**
  * Check if we're required to add a port number.
  *
- * @see https://url.spec.whatwg.org/#default-port
+ * @see //url.spec.whatwg.org/#default-port
  * @param {Number|String} port Port number we need to check
  * @param {String} protocol Protocol we need to check against.
  * @returns {Boolean} Is it a default port for the given protocol
