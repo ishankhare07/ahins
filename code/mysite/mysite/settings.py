944e98ff4177b8473079ac1fd82dac5dce8cd7b5
Ishan Khare <ishankhare07@gmail.com>
2019-08-08 13:26:40 +0530

    order blog posts by reverse published date
    turn off debug flag in prod
    rename cert names in nginx conf
    
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- code/mysite/mysite/settings.py
+++ code/mysite/mysite/settings.py
@@ -27,7 +27,7 @@ if os.environ.get('IS_DEV_ENV'):
     DEBUG = True
     ALLOWED_HOSTS = ['localhost']
 else:
-    DEBUG = True
+    DEBUG = False
     ALLOWED_HOSTS = ['35.188.215.192', 'ishankhare.com', 'www.ishankhare.com', 'localhost', 'staging.ishankhare.com']
 
 
