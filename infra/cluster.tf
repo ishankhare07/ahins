c104d7642af0e6e625369422fb759c01a9c037b8
Ishan Khare <ishankhare07@gmail.com>
2020-07-05 12:10:31 +0530

    make cluster vpc-native
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -1,37 +1,49 @@
 provider "google" {
 
-    project = "tonal-baton-181908"
-    region = "asia-south1"
-    zone = "asia-south1-a"
+  project = "tonal-baton-181908"
+  region  = "asia-south1"
+  zone    = "asia-south1-a"
 }
 
 resource "google_container_cluster" "primary" {
-    name = "ahins"
-    location = "asia-south1"
+  name     = "ahins"
+  location = "asia-south1"
 
-    initial_node_count = 1
+  initial_node_count        = 1
+  default_max_pods_per_node = 32
 
-    master_auth {
-        username = ""
-        password = ""
+  master_auth {
+    username = ""
+    password = ""
 
-        client_certificate_config {
-            issue_client_certificate = false
-        }
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  node_config {
+    machine_type = "e2-micro"
+    disk_size_gb = "10"
+    image_type   = "cos_containerd"
+
+    metadata = {
+      disable-legacy-endpoints = true
     }
 
-    node_config {
-        machine_type = "e2-micro"
-        disk_size_gb = "10"
-        image_type = "cos_containerd"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
 
-        metadata = {
-            disable-legacy-endpoints = true
-        }
+  private_cluster_config {
+    enable_private_nodes = true
+    enable_private_endpoint = false
+    master_ipv4_cidr_block = var.node_ip_range
+  }
 
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/logging.write",
-            "https://www.googleapis.com/auth/monitoring",
-        ]
-    }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = var.pod_ip_range
+    services_ipv4_cidr_block = var.service_ip_range
+  }
 }
