4ffca956d8ec00a87291b27f61e6bf246db5858e
Ishan Khare <ishankhare07@gmail.com>
2020-07-17 15:43:45 +0530

    change network tier for VM
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- infra-box/box.tf
+++ infra-box/box.tf
@@ -13,7 +13,8 @@ resource "google_compute_instance" "ahins" {
     network = "default"
 
     access_config {
-      nat_ip = google_compute_address.static.address
+      nat_ip       = google_compute_address.static.address
+      network_tier = "STANDARD"
     }
   }
 
