c9fc0c541b6baa382e65a7709b2a0030ffcda70b
Ishan Khare <ishankhare07@gmail.com>
2020-07-17 13:25:18 +0530

    add terraform and ci job for box
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

---
+++ infra-box/box.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "ahins" {
+  name = "test"
+  machine_type = "f1-micro"
+  zone = "asia-south1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2004-lts/ubuntu-minimal-2004-focal-v20200702"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
