ab01dec13ce1f6abee4d4503dedbcaddcee18ad6
Ishan Khare <ishankhare07@gmail.com>
2020-07-25 09:44:58 +0000

    Update post_ssh/setup_docker.sh

--- post_ssh/setup_docker.sh
+++ post_ssh/setup_docker.sh
@@ -1,18 +1,18 @@
-apt-get update
-apt-get install -y \
-    apt-transport-https \
+sudo apt-get update
+sudo apt-get install -y \
+    sudo apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-add-apt-repository \
+sudo add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 
-apt update
-apt install -y docker-ce docker-ce-cli 
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli 
