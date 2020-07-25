fa4b0e8c8683564217dc9cd94c3d537ba12eb7d0
Ishan Khare <ishankhare07@gmail.com>
2020-07-25 14:15:14 +0000

    Update post_ssh/setup_docker.sh

--- post_ssh/setup_docker.sh
+++ post_ssh/setup_docker.sh
@@ -17,3 +17,7 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli 
 sudo usermod -aG docker ishankhare
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
