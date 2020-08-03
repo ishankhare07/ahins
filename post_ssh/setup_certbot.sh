d8bca96adef2e4f7c64ac71d927c9b4c708e3995
Ishan Khare <ishankhare07@gmail.com>
2020-08-03 16:50:24 +0000

    add non interactive certbot

--- post_ssh/setup_certbot.sh
+++ post_ssh/setup_certbot.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository universe
 sudo apt-get update
 
 sudo apt-get install -y certbot python3-certbot-nginx
+
+sudo certbot --nginx -n --domain ishankhare.dev --redirect --agree-tos -m me@ishankhare.dev
