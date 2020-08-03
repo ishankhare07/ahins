6d2bc2f5f2e2fdc41740e6bbc662b29585d33197
Ishan Khare <ishankhare07@gmail.com>
2020-08-03 16:16:33 +0000

    fix setup_certbot script

--- post_ssh/setup_certbot.sh
+++ post_ssh/setup_certbot.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository universe
 sudo apt-get update
 
-sudo apt-get install certbot python3-certbot-nginx
+sudo apt-get install -y certbot python3-certbot-nginx
