420ab91c40345418a1554443b9edd56c773a5bce
Ishan Khare <ishankhare07@gmail.com>
2020-08-23 08:32:22 +0000

    add cluster resource

--- infra/civo/cluster.tf
+++ infra/civo/cluster.tf
@@ -1,3 +1,10 @@
 provider "civo" {
 
 }
+
+resource "civo_kubernetes_cluster" "ahins" {
+    name = "ahins"
+    applications = "Traefik, Metrics"
+    num_target_nodes = 4
+    target_nodes_size = element(data.civo_instances_size.small.sizes, 0).name
+}
