1fae219c3e601b945ecfef82aab469df88d6243d
Ishan Khare <ishankhare07@gmail.com>
2020-08-23 09:20:35 +0000

    fix terraform provider plugin

--- infra/civo/cluster.tf
+++ infra/civo/cluster.tf
@@ -6,5 +6,5 @@ resource "civo_kubernetes_cluster" "ahins" {
     name = "ahins"
     applications = "Traefik, Metrics"
     num_target_nodes = 4
-    target_nodes_size = element(data.civo_instances_size.small.sizes, 0).name
+    target_nodes_size = "g2.small"
 }
