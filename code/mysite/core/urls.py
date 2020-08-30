8dafd00415189b3d32d02e5ad015937961b7e975
Ishan Khare <ishankhare07@gmail.com>
2020-08-30 23:46:28 +0530

    update rest framework and fix login/logout views
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- code/mysite/core/urls.py
+++ code/mysite/core/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-        url(r'^login/$', auth_views.login, name="login", kwargs={'redirect_authenticated_user': True}),
-        url(r'^logout/$', auth_views.logout, name="logout")
+        url(r'^login/$', auth_views.LoginView, name="login", kwargs={'redirect_authenticated_user': True}),
+        url(r'^logout/$', auth_views.LogoutView, name="logout", kwargs={'next_page': '/'})
         ]
